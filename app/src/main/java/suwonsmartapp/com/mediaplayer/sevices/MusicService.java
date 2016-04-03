fea12aaed15dc0157e28ece277b0dff55784addd
Junsuk Oh <a811219@gmail.com>
2016-04-03 20:54:39 +0900

    foreground service 로 음악 재생

--- app/src/main/java/suwonsmartapp/com/mediaplayer/sevices/MusicService.java
+++ app/src/main/java/suwonsmartapp/com/mediaplayer/sevices/MusicService.java
@@ -1,39 +1,62 @@
 package suwonsmartapp.com.mediaplayer.sevices;
 
-import android.app.IntentService;
+import android.app.Notification;
+import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.widget.Toast;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
 
-import java.io.IOException;
+import suwonsmartapp.com.mediaplayer.R;
 
-public class MusicService extends IntentService {
+public class MusicService extends Service {
 
     private MediaPlayer mMediaPlayer;
 
-    public MusicService() {
-        super("MusicService");
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
         mMediaPlayer = new MediaPlayer();
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+        builder.setContentTitle("재생 중");
+        builder.setContentText("아무 음악 제목임");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        Notification notification = builder.build();
+
+
+//        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+//        notificationManagerCompat.notify(1, notification);
+
+        startForeground(1, notification);
+
         Uri uri = intent.getParcelableExtra("uri");
-        try {
-            mMediaPlayer.setDataSource(getApplicationContext(), uri);
-            mMediaPlayer.prepare();
 
-            if (mMediaPlayer.isPlaying()) {
-                mMediaPlayer.stop();
-            } else {
-                mMediaPlayer.start();
-            }
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
 
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "문제가 있습니다", Toast.LENGTH_SHORT).show();
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.stop();
+        } else {
+
+            mMediaPlayer.start();
+
         }
+
+        return START_REDELIVER_INTENT;
     }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
 
 }
